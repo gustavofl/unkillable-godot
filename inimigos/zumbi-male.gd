3f8038a874f6dd3285127a985330577439d7e3bf
Gustavo <gustavofernandezluz@gmail.com>
2019-11-28 15:19:00 -0300

    Barra de vida inimiga e animação de dano

--- inimigos/zumbi-male.gd
+++ inimigos/zumbi-male.gd
@@ -1,34 +1,73 @@
 extends StaticBody2D
 
-const speed = 0.2
+const speed = 0.4
+const max_life = 10
+
 var dano = 5
 var flip = true
 var posicao_inicial
 var posicao_final
+var life = max_life
 
+var tomando_dano = false
 var morrendo = false
 
 func _ready():
 	$Sprite.play("walk");
 	posicao_inicial = $".".position.x
 	posicao_final = posicao_inicial + 100
+	
+	$healthBar.max_value = max_life
+	$healthBar.value = life
 
 func _process(delta):
-	if($".".position.x <= posicao_final and flip):
-		$".".position.x += speed
+	if flip:
+		if tomando_dano:
+			$".".position.x -= speed
+		else:
+			$".".position.x += speed
+			
 		$Sprite.flip_h = false
-		if($".".position.x >= posicao_final):
-			flip = false
-	
-	if($".".position.x >= posicao_inicial and !flip):
-		$".".position.x -= speed
+	else:
+		if tomando_dano:
+			$".".position.x += speed
+		else:
+			$".".position.x -= speed
+		
 		$Sprite.flip_h = true
-		if($".".position.x <= posicao_inicial):
-			flip = true
 
-func dano():
+func dano(body, amount):
+	tomando_dano = true
+	
+	if body.get_position().x < get_position().x:
+		flip = false
+	else:
+		flip = true
+	
+	life -= amount
+	$healthBar.value = life
+	
+	if life <= 0:
+		die()
+	else:
+		$Sprite.play("hurt")
+		get_node("anim").play("hurt")
+
+func die():
 	morrendo = true
 	get_node("anim").play("die")
 
-func die():
+func destroy():
 	$".".queue_free()
+
+func _on_chao_direita_body_exited(body):
+	flip = true
+
+func _on_chao_esquerda_body_exited(body):
+	flip = false
+
+
+func _on_anim_animation_finished(anim_name):
+	if(anim_name == "hurt"):
+		tomando_dano = false
+		$Sprite.play("walk")
--- player/player.gd
+++ player/player.gd
@@ -2,18 +2,24 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const SPEED = 200
-const GRAVITY = 17000
-const JUMP_HEIGHT = 14000
+const GRAVITY = 43000
+const JUMP_HEIGHT = 25000
+const DASH = 150
 
 var speed_y = 0
 var motion = Vector2()
 var max_life = 20
 var life
-var ataque = false
+var forca_ataque = 5
+
+var sprite_travado = false
+var atacando = false
+var tomando_dano = false
 
 func _ready():
 	life = max_life
-	$ataque/shape.disabled = true
+	$ataque_direita/shape.disabled = true
+	$ataque_esquerda/shape.disabled = true
 
 func _physics_process(delta):
 	
@@ -24,7 +30,25 @@ func _physics_process(delta):
 	else:
 		motion.x = 0
 	
-	if $Sprite.animation != "attack":
+	if sprite_travado:
+		if atacando:
+			if($Sprite.flip_h):
+				motion.x = -DASH
+			else:
+				motion.x = DASH
+		elif tomando_dano:
+			if($Sprite.flip_h):
+				motion.x = DASH
+			else:
+				motion.x = -DASH
+	else:
+		if Input.is_action_pressed("ui_right"):
+			motion.x = SPEED
+		elif Input.is_action_pressed("ui_left"):
+			motion.x = -SPEED
+		else:
+			motion.x = 0
+		
 		if Input.is_action_pressed("ui_right"):
 			$Sprite.play("run")
 			$Sprite.flip_h = false
@@ -36,10 +60,15 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_pressed("ui_atack"):
 		$Sprite.play("attack")
-		$ataque/shape.disabled = false
+		if $Sprite.flip_h:
+			$ataque_esquerda/shape.disabled = false
+		else:
+			$ataque_direita/shape.disabled = false
+		sprite_travado = true
+		atacando = true
 		
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
+		if Input.is_action_just_pressed("ui_up"):
 			speed_y = -JUMP_HEIGHT
 		else:
 			speed_y = 0
@@ -47,17 +76,13 @@ func _physics_process(delta):
 		$Sprite.play("jump")
 	
 	if is_on_ceiling():
-		speed_y = -speed_y
+		speed_y = 0
 	
 	speed_y += GRAVITY * delta
 	
 	motion.y = speed_y * delta
 	
 	motion = move_and_slide(motion, UP)
-
-func _on_pes_body_entered(body):
-	body.dano()
-	speed_y = -JUMP_HEIGHT
 
 func _on_dano_body_entered(body):
 	if(body.morrendo):
@@ -67,6 +92,10 @@ func _on_dano_body_entered(body):
 	
 	if life <= 0:
 		get_tree().change_scene("res://cenas/menu.tscn")
+	else:
+		$Sprite.play("hurt")
+		sprite_travado = true
+		tomando_dano = true
 
 func increment_life(amount):
 	var healthBar = $"../GUI/interface/VBoxContainer/Healthbar/TextureProgress"
@@ -74,10 +103,23 @@ func increment_life(amount):
 	healthBar.value = life
 
 func _on_Sprite_animation_finished():
-	if $Sprite.animation == "attack":
+	if atacando:
 		$Sprite.play("idle")
-		$ataque/shape.disabled = true
+		if $Sprite.flip_h:
+			$ataque_esquerda/shape.disabled = true
+		else:
+			$ataque_direita/shape.disabled = true
+		sprite_travado = false
+		atacando = false
+	elif tomando_dano:
+		$Sprite.play("idle")
+		sprite_travado = false
+		tomando_dano = false
 
-func _on_ataque_body_entered(body):
+func _on_ataque_direita_body_entered(body):
 	$".".add_collision_exception_with(body)
-	body.dano()
+	body.dano(self, forca_ataque)
+
+func _on_ataque_esquerda_body_entered(body):
+	$".".add_collision_exception_with(body)
+	body.dano(self, forca_ataque)
